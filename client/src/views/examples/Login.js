cfb251698ad0563379bf3e12c2880ee99f60bf94
Ayush Rana <ranaayush900@gmail.com>
2023-11-03 19:26:10 -0400

    Updated overall UI,Added Interactions,Added Footer

--- client/src/views/examples/Login.js
+++ client/src/views/examples/Login.js
@@ -158,5 +158,4 @@ class Login extends React.Component {
     );
   }
 }
-
 export default Login;
--- client/src/views/examples/Main.js
+++ client/src/views/examples/Main.js
@@ -1,8 +1,9 @@
 import React from "react";
 // nodejs library that concatenates classes
 import classnames from "classnames";
-import backgroundImage from '../../assets/img/brand/banner2.jpg';
-
+import backgroundImage from '../../assets/img/brand/banner5.jpg';
+import logo from "../../assets/img/brand/Logo.png"
+import SimpleFooter from "components/Footers/SimpleFooter.js";
 
 // reactstrap components
 import {
@@ -33,9 +34,21 @@ class Main extends React.Component {
         super(props);
         this.state = {
           companySector: [
-            { id: 1, name: 'A' },
-            { id: 2, name: 'B' },
-            { id: 3, name: 'C' },
+            { id: 1, name: 'Agriculture' },
+            { id: 2, name: 'Residential' },
+            { id: 3, name: 'Commercial Real Estate' },
+            { id: 4, name: 'College/Universities' },
+            { id: 5, name: 'Food Processing' },
+            { id: 6, name: 'Dairy' },
+            { id: 7, name: 'Electrification' },
+            { id: 8, name: 'Grocery' },
+            { id: 9, name: 'Greenhouses' },
+            { id: 10, name: 'Health Care' },
+            { id: 11, name: 'Hospitality' },
+            { id: 12, name: 'Parking Garage/Lots' },
+            { id: 13, name: 'Retail' },
+            { id: 14, name: 'Senior Housing' },
+            { id: 15, name: 'C' },
           ],
 
           postalCode: [
@@ -45,18 +58,27 @@ class Main extends React.Component {
           ],
 
           employeeCount: [
-            { id: 1, name: 'A' },
-            { id: 2, name: 'B' },
-            { id: 3, name: 'C' },
+            { id: 1, name: '< 20' },
+            { id: 2, name: '20 - 49' },
+            { id: 3, name: '50 - 249' },
+            { id: 3, name: '250 +' },
           ],
 
           electricityBudget: [
-            { id: 1, name: 'A' },
-            { id: 2, name: 'B' },
-            { id: 3, name: 'C' },
+            { id: 1, name: '<$ 50k' },
+            { id: 2, name: '$ 50k-$100k' },
+            { id: 3, name: '$ 100k - $ 250k' },
+            { id: 3, name: '$ 250k+' },
           ],
 
           gasBudget: [
+            { id: 1, name: '<$ 50k' },
+            { id: 2, name: '$ 50k - $100k' },
+            { id: 3, name: '$ 100k - $250k' },
+            { id: 3, name: '$ 250k+' }
+          ],
+
+          energyConsumption: [
             { id: 1, name: 'A' },
             { id: 2, name: 'B' },
             { id: 3, name: 'C' },
@@ -72,6 +94,16 @@ class Main extends React.Component {
         document.scrollingElement.scrollTop = 0;
         this.refs.main.scrollTop = 0;
     }
+
+    scrollToSearch = () => {
+        // Find the search section element
+        const searchSection = document.getElementById('searchSection');
+    
+        // Scroll to the search section smoothly
+        if (searchSection) {
+          searchSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
 
     render() {
         const bgStyle = {
@@ -89,25 +121,16 @@ class Main extends React.Component {
                         {/* shape Hero */}
                         <section className="section section-lg section-shaped pb-250">
                             <div className="shape shape-style-1 shape-default">
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span />
+                              
                         
                             </div>
                             <Container className="py-lg-md d-flex">
                                 <div className="col px-0">
                                     <Row>
                                         <Col lg="6">
-                                            <h1 className="display-3 text-white">
+                                            <h1 className="display-1 text-white">
                                                 GreenThumb{" "}
-                                                <span>Funding for Energy Projects (Business)</span>
+                                                <span className="display-3 text-white">Funding for Energy Projects</span>
                                             </h1>
                                             <p className="lead text-white">
                                                 Empowering companies to unlock financial opportunities through targeted program discovery and funding eligibility – let's build a prosperous future together!
@@ -115,15 +138,32 @@ class Main extends React.Component {
 
                                         </Col>
                                     </Row>
+                                    <Row>
+                                        <Col>
+        
+                                            <Button
+                                                    className="mt-4"
+                                                    color="success"
+                                                    href="#pablo"
+                                                    onClick={this.scrollToSearch}
+                                                    style={{ width: '50%', backgroundColor: '#40C365', border: '0px solid #'}}
+                                                >
+                                                     Search your Programs
+                                                </Button>
+
+                                        </Col>
+                                        <Col>
+
+                                        </Col>
+                                    </Row>
                                 </div>
                             </Container>
-                            {/* SVG separator */}
 
                         </section>
-                        {/* 1st Hero Variation */}
+                       
                     </div>
 
-                    <section className="section section-lg">
+                    <section className="section section-lg" id="searchSection">
                         <Container>
                             <Row className="row-grid align-items-center">
                                 <Col className="order-md-2" md="6">
@@ -131,11 +171,11 @@ class Main extends React.Component {
                                         <Card className="card-lift--hover shadow border-0">
                                             <CardBody className="py-5">
                                                 <Col className="order-md-2" md="12" style={{ padding: "10px" }}>
-                                                    <UncontrolledDropdown style={{ width: "100%" }}>
+                                                    <UncontrolledDropdown style={{ width: "100%" }} > 
                                                         <DropdownToggle caret color="secondary" style={{ width: "100%" }}>
                                                             Company Sector
                                                         </DropdownToggle>
-                                                        <DropdownMenu style={{ width: "100%" }}>
+                                                        <DropdownMenu style={{ width: "100%" }} className="scrollable-dropdown">
                                                         {this.state.companySector.map(item => (
                                                             <>
                                                                
@@ -230,6 +270,26 @@ class Main extends React.Component {
                                                         </DropdownMenu>
                                                     </UncontrolledDropdown>
                                                 </Col>
+                                                <Col className="order-md-2" md="12" style={{ padding: "10px" }}>
+                                                    <UncontrolledDropdown style={{ width: "100%" }}>
+                                                        <DropdownToggle caret color="secondary" style={{ width: "100%" }}>
+                                                            Minimum Energy Consumption
+                                                        </DropdownToggle>
+                                                        <DropdownMenu style={{ width: "100%" }}>
+                                                        {this.state.energyConsumption.map(item => (
+                                                            <>
+                                                               
+                                                                    <DropdownItem href="#pablo" onClick={e => e.preventDefault()} 
+                                                                     key={item.id}
+                                                                    >
+                                                                    {item.name}
+                                                                    </DropdownItem>
+                                                                
+                                                            </>
+                                                        ))}
+                                                        </DropdownMenu>
+                                                    </UncontrolledDropdown>
+                                                </Col>
                                                 <Button
                                                     className="mt-4"
                                                     color="success"
@@ -247,45 +307,11 @@ class Main extends React.Component {
                                 <Col className="order-md-1" md="6">
                                     <div className="pr-md-5">
                                         <div className="icon icon-lg icon-shape icon-shape-success shadow rounded-circle mb-5">
-                                            <i className="ni ni-settings-gear-65" />
+                                            <i className="ni ni-like-2" />
                                         </div>
                                         <h3>Search your program</h3>
-                                        <p>
-                                           
-                                        </p>
+                                        
                                         <ul className="list-unstyled mt-5">
-                                            <li className="py-2">
-                                                <div className="d-flex align-items-center">
-                                                    <div>
-                                                        <Badge
-                                                            className="badge-circle mr-3"
-                                                            color="success"
-                                                        >
-                                                            <i className="ni ni-settings-gear-65" />
-                                                        </Badge>
-                                                    </div>
-                                                    <div>
-                                                        <h6 className="mb-0">
-                                                            
-                                                        </h6>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            <li className="py-2">
-                                                <div className="d-flex align-items-center">
-                                                    <div>
-                                                        <Badge
-                                                            className="badge-circle mr-3"
-                                                            color="success"
-                                                        >
-                                                            <i className="ni ni-html5" />
-                                                        </Badge>
-                                                    </div>
-                                                    <div>
-                                                        <h6 className="mb-0"></h6>
-                                                    </div>
-                                                </div>
-                                            </li>
                                             <li className="py-2">
                                                 <div className="d-flex align-items-center">
                                                     <div>
@@ -298,7 +324,41 @@ class Main extends React.Component {
                                                     </div>
                                                     <div>
                                                         <h6 className="mb-0">
-                                                            Super friendly support team
+                                                            Energy efficient tips
+                                                        </h6>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li className="py-2">
+                                                <div className="d-flex align-items-center">
+                                                    <div>
+                                                        <Badge
+                                                            className="badge-circle mr-3"
+                                                            color="success"
+                                                        >
+                                                            <i className="ni ni-world-2" />
+                                                        </Badge>
+                                                    </div>
+                                                    <div>
+                                                        <h6 className="mb-0">
+                                                            Discover your options here
+                                                        </h6>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li className="py-2">
+                                                <div className="d-flex align-items-center">
+                                                    <div>
+                                                        <Badge
+                                                            className="badge-circle mr-3"
+                                                            color="success"
+                                                        >
+                                                            <i className="ni ni-ui-04" />
+                                                        </Badge>
+                                                    </div>
+                                                    <div>
+                                                        <h6 className="mb-0">
+                                                            Filter out your Eligibility criteria
                                                         </h6>
                                                     </div>
                                                 </div>
@@ -349,11 +409,11 @@ class Main extends React.Component {
                                                 <InputGroup className="input-group-alternative">
                                                     <InputGroupAddon addonType="prepend">
                                                         <InputGroupText>
-                                                            <i className="ni ni-user-run" />
+                                                            <i className="ni ni-building" />
                                                         </InputGroupText>
                                                     </InputGroupAddon>
                                                     <Input
-                                                        placeholder="Your name"
+                                                        placeholder="Company name"
                                                         type="text"
                                                         onFocus={(e) =>
                                                             this.setState({ nameFocused: true })
@@ -416,7 +476,7 @@ class Main extends React.Component {
                     </section>
 
                 </main>
-
+                <SimpleFooter/>
             </>
         );
     }
